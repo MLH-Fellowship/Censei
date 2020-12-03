12676541d2acab421507d4dd2b4589fe2f178b0b
Ajwad Shaikh <shaikhajwad10@gmail.com>
2020-12-03 16:06:59 +0530

    Setup backend flask project

---
+++ censei-backend/main.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
+import os
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route('/ping', methods=['GET'])
+@cross_origin()
+def parseFile():
+    return 'pong'
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
