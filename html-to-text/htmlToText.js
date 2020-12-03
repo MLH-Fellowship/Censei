988e8bb0b63620aea6dbf023d12752650ba221cc
AhadKhan98 <ahad_zai@berea.edu>
2020-12-03 22:28:19 +0500

    refactor htmltotext.js and add steps

--- html-to-text/htmlToText.js
+++ html-to-text/htmlToText.js
@@ -1,3 +1,15 @@
-const bodyText = document.body.innerText;
+/* 
+Steps:
+    1) Get all text from body
+    2) send resulting text to backend
+    3) Receive the updated text from backend
+    4) Replace body text with the new text
+*/
 
-return bodyText;
+
+// Get text from body
+const getBodyText = () => {
+    const bodyText = document.body.innerText;
+    return bodyText;
+};
+
