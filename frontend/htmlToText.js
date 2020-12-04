ff92b08ffb4d3d13018814cc6ae05b5a2b818b46
AhadKhan98 <ahad_zai@berea.edu>
2020-12-04 11:40:51 +0500

    add template code to sendToBackend function

--- frontend/htmlToText.js
+++ frontend/htmlToText.js
@@ -16,9 +16,22 @@ const getBodyText = () => {
 // Send text to backend
 const sendToBackendAndWaitForResponse = () => {
 
-     //TODO: Handle posting data here
+    // Post body text to backend
+    let newBodyText = '';
+    bodyText = getBodyText();
 
+    fetch('http://127.0.0.1:8080/post', {method: 'POST', body:bodyText})
+    .then((response) => {
+        // Resulting clean text is collected here
+        console.log('Response from backend = ', response)
+        newBodyText = response;
+    })
+    .catch((err) => {
+        console.log('Error: ', err);
+    });
 
+    return newBodyText;
+    
 };
 
 // Replace body text
