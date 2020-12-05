6be322cf3ff2f6ff624c5d08af6375a282121ef1
Ahad Zai <34427125+AhadKhan98@users.noreply.github.com>
2020-12-05 11:48:39 -0800

    Censor text from html page (#8)
    * add html-to-text folder
    
    * remove index
    
    * refactor htmltotext.js and add steps
    
    * create replaceBodyText function
    
    * remove index.html
    
    * merge send and receive functions into one
    
    * create replaceBodyText function
    
    * fix directory structure
    
    * add template code to sendToBackend function
    
    * fix todo in replaceBodyText function
    
    * connect backend to htmlToText function
    
    * remove instructions from top of file
    
    * refactor the code

---
+++ frontend/htmlToText.js
@@ -0,0 +1,48 @@
+
+// Get text from body
+const getBodyText = () => {
+    const bodyText = document.body.innerHTML;
+    return bodyText;
+};
+
+// Send text to backend and get censored text
+const sendToBackendAndWaitForResponse = () => {
+
+    // Post body text to backend
+    let newBodyText = '';
+    bodyText = getBodyText();
+
+    return fetch('http://localhost:8080/censorText', {
+        method: 'POST', 
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({text: bodyText})
+    })
+    .then((response) => {
+        // Resulting clean text is collected here
+        return response.json()
+    })
+    .then(json => {
+        console.log(json.censored_text)
+        newBodyText = json.censored_text;
+        return newBodyText;
+    })
+    .catch((err) => {
+        console.log('Error: ', err);
+        return;
+    });
+    
+};
+
+// Replace body text
+const replaceBodyText = () => {
+    sendToBackendAndWaitForResponse()
+    .then(censoredText => {
+        document.body.innerHTML = censoredText;
+    })
+    
+};
+
+let censorButton = document.getElementById('censorButton');
+censorButton.addEventListener('click', replaceBodyText);
