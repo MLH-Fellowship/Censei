a9b0ba94823d0b5d4cb3a4a9fc1a1f919321d33a
Ajwad Shaikh <shaikhajwad10@gmail.com>
2020-12-06 21:43:17 +0530

    Added Dockerfile to build and run docker images

--- censei-backend/app.py
+++ censei-backend/app.py
@@ -9,15 +9,12 @@ app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-emojis = "🍏 🍎 🍐 🍊 🍋 🍌 🍉 🍇 🍓 🍈 🍒 🍑 🍍 🥝 🍅 🍆 🥑 🥒 🌶 🌽 🥕 🥔 🍠 🥐 🍞 🥖 🧀 🥚 🍳 🥞 🥓 🍗 🍖 🌭 🍔 🍟 🍕 🥙 🌮 🌯 🥗 🥘 🍝 🍜 🍲 🍛 🍣 🍱 🍤 🍙 🍚 🍘 🍥 🍢 🍡 🍧 🍨 🍦 🍰 🎂 🍮 🍭 🍬 🍫 🍿 🍩 🍪 🌰 🥜 🍯 🥛 🍼 🍵 🍶 🍺 🍻 🥂 🍷 🥃 🍸 🍹 🍾".split(
-    ' ')
-
+emojis = "🍏 🍎 🍐 🍊 🍋 🍌 🍉 🍇 🍓 🍈 🍒 🍑 🍍 🥝 🍅 🍆 🥑 🥒 🌶 🌽 🥕 🥔 🍠 🥐 🍞 🥖 🧀 🥚 🍳 🥞 🥓 🍗 🍖 🌭 🍔 🍟 🍕 🥙 🌮 🌯 🥗 🥘 🍝 🍜 🍲 🍛 🍣 🍱 🍤 🍙 🍚 🍘 🍥 🍢 🍡 🍧 🍨 🍦 🍰 🎂 🍮 🍭 🍬 🍫 🍿 🍩 🍪 🌰 🥜 🍯 🥛 🍼 🍵 🍶 🍺 🍻 🥂 🍷 🥃 🍸 🍹 🍾".split(' ')
 
 @app.route('/ping', methods=['GET'])
 @cross_origin()
 def pong():
     return 'pong'
-
 
 @app.route('/censorText', methods=['POST'])
 @cross_origin()
@@ -30,10 +27,8 @@ def censorText():
     words = text.split(' ')
     censored_text = ''
     for word in words:
-        censored_text += (" " + (profanity.censor(word,
-                                                  censor_char=random.choice(emojis))))
-    return {"censored_text": censored_text}
-
+        censored_text += (" " + (profanity.censor(word, censor_char=random.choice(emojis))))
+    return { "censored_text": censored_text }
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
