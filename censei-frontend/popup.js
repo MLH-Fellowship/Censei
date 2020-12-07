b0bb3af40c9821e0dc4907df35f7557d94df0acb
AhadKhan98 <ahad_zai@berea.edu>
2020-12-07 13:38:05 +0500

    initialize localStorage variables on first run

--- censei-frontend/popup.js
+++ censei-frontend/popup.js
@@ -1,1 +1,18 @@
-alert("HIIII");
+let wordsCensored = localStorage.getItem('wordsCensored');
+
+const initFirstRun = () => {
+    if (!wordsCensored) {
+        localStorage.setItem('wordsCensored','0');
+        wordsCensored = localStorage.getItem('wordsCensored');
+    }
+}
+
+const updateCounterElement = () => {
+    const counterElement = document.getElementById("counter");
+    counterElement.innerText = wordsCensored
+}
+
+initFirstRun();
+updateCounterElement();
+
+
